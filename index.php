98737527a38fe7e581c8cbbb8a6f8da3690e442b
Petr Rostov <petr.rostov@gmail.com>
2011-08-26 17:53:09 +0400

    Первый коммит

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Депрессивный андроид</title>
+<link rel="shortcut icon" href="img/favicon.ico" />
+<link type="text/css" rel="stylesheet" href="css/style.css" />
+<!--[if lte IE 8]>
+	<link type="text/css" rel="stylesheet" href="css/ie.css" />
+	<script type="text/javascript" src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+	<section>
+		<header>
+			<div id="logo"></div>
+			<nav>
+				<ul>
+					<li><a href="javascript:void(0)">Мы</a></li>
+					<li class="current">Что мы делаем</li>
+					<li><a href="javascript:void(0)">Связаться с нами</a></li>
+				</ul>
+			</nav>
+		</header>
+		<article>
+			<p>Суж&shy;де&shy;ние ос&shy;по&shy;соб&shy;ля&shy;ет слож&shy;ный ба&shy;бу&shy;визм, не&nbsp;учи&shy;ты&shy;вая мне&shy;ния ав&shy;то&shy;ри&shy;те&shy;тов. За&shy;кон ис&shy;клю&shy;чён&shy;но&shy;го треть&shy;е&shy;го не&shy;пред&shy;взя&shy;то рас&shy;смат&shy;ри&shy;ва&shy;ет&shy;ся при&shy;ми&shy;тив&shy;ный за&shy;кон внеш&shy;не&shy;го ми&shy;ра, от&shy;ри&shy;цая оче&shy;вид&shy;ное. Ге&shy;ний тво&shy;рит слож&shy;ный ин&shy;тел&shy;лект, ло&shy;мая рам&shy;ки при&shy;выч&shy;ных пред&shy;став&shy;ле&shy;ний. Мож&shy;но пред&shy;по&shy;ло&shy;жить, что пла&shy;то&shy;нов&shy;ская ака&shy;де&shy;мия ам&shy;би&shy;ва&shy;лент&shy;но дис&shy;кре&shy;ди&shy;ти&shy;ру&shy;ет из&nbsp;ря&shy;да вон вы&shy;хо&shy;дя&shy;щий ба&shy;бу&shy;визм, ло&shy;мая рам&shy;ки при&shy;выч&shy;ных пред&shy;став&shy;ле&shy;ний.</p>
+			<p>Суж&shy;де&shy;ние ос&shy;по&shy;соб&shy;ля&shy;ет слож&shy;ный ба&shy;бу&shy;визм, не&nbsp;учи&shy;ты&shy;вая мне&shy;ния ав&shy;то&shy;ри&shy;те&shy;тов. За&shy;кон ис&shy;клю&shy;чён&shy;но&shy;го треть&shy;е&shy;го не&shy;пред&shy;взя&shy;то рас&shy;смат&shy;ри&shy;ва&shy;ет&shy;ся при&shy;ми&shy;тив&shy;ный за&shy;кон внеш&shy;не&shy;го ми&shy;ра, от&shy;ри&shy;цая оче&shy;вид&shy;ное. Ге&shy;ний тво&shy;рит слож&shy;ный ин&shy;тел&shy;лект, ло&shy;мая рам&shy;ки при&shy;выч&shy;ных пред&shy;став&shy;ле&shy;ний. Мож&shy;но пред&shy;по&shy;ло&shy;жить, что пла&shy;то&shy;нов&shy;ская ака&shy;де&shy;мия ам&shy;би&shy;ва&shy;лент&shy;но дис&shy;кре&shy;ди&shy;ти&shy;ру&shy;ет из&nbsp;ря&shy;да вон вы&shy;хо&shy;дя&shy;щий ба&shy;бу&shy;визм, ло&shy;мая рам&shy;ки при&shy;выч&shy;ных пред&shy;став&shy;ле&shy;ний.</p>
+		</article>	
+	</section>
+</body>
+</html>
